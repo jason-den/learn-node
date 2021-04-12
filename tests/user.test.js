257af8c72abb4553cb606dfa46ca97634140fad6
Jason Den <jason.den.cloud@gmail.com>
2021-04-12 10:44:25 +1000

    Lesson 9: Testing with Authentication

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,11 +1,16 @@
+const { Types } = require('mongoose')
 const request = require('supertest')
 const app = require('../src/app')
 const User = require('../src/models/user')
+const jwt = require('jsonwebtoken')
 
+const userOneId = new Types.ObjectId()
 const userOne = {
+  _id: userOneId,
   name: 'Jason',
   email: 'jason.den@example.com',
   password: 'MyPass77@#',
+  tokens: [{ token: jwt.sign({ _id: userOneId }, process.env.JWT_SECRET) }],
 }
 const userTwo = {
   name: 'Mark',
@@ -24,4 +29,37 @@ test('Should signup a new user', async () => {
 })
 test('Should login an exsisting user', async () => {
   await request(app).post('/users/login').send(userOne).expect(200)
+})
+describe('Should not login with wrong credentials', () => {
+  test('wrong password', async () => {
+    await request(app)
+      .post('/users/login')
+      .send({ email: userOne.email, password: 'the wrong password' })
+      .expect(400)
+  })
+  test('nonexisting email', async () => {
+    await request(app)
+      .post('/users/login')
+      .send({ email: 'nobody@whatever.com', password: 'nopenope' })
+      .expect(400)
+  })
+})
+
+describe('on /users/me', () => {
+  test('Should get profile for authenticated user', async () => {
+    await request(app)
+      .get('/users/me')
+      .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+      .send()
+      .expect(200)
+  })
+  test('Should not get profile for unauthenticated user', async () => {
+    await request(app)
+      .get('/users/me')
+      .set('Authorization', `wrong token`)
+      .send()
+      .expect(401)
+
+    await request(app).get('/users/me').send().expect(401)
+  })
 })
